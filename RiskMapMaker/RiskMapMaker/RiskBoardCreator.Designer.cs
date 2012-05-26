a9c83fb0eb24b11a02520152411de6184e587990
Quentin Brooks <yeerkkiller1@gmail.com>
2012-05-26 00:25:11 -0400

    RiskMapMaker 50 percent done

---
+++ RiskMapMaker/RiskMapMaker/RiskBoardCreator.Designer.cs
@@ -0,0 +1,168 @@
+﻿using System.Windows.Forms;
+namespace RiskMapMaker
+{
+    partial class BoardCreator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BoardCreator));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.ClearCurrent = new System.Windows.Forms.Button();
+            this.Export = new System.Windows.Forms.Button();
+            this.ClearLast = new System.Windows.Forms.Button();
+            this.createCountries = new System.Windows.Forms.RadioButton();
+            this.createConnections = new System.Windows.Forms.RadioButton();
+            this.Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Image = global::RiskMapMaker.Properties.Resources.riskmap_en_small;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(850, 650);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(12, 669);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(137, 23);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "Add (a)";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // ClearCurrent
+            // 
+            this.ClearCurrent.Location = new System.Drawing.Point(155, 669);
+            this.ClearCurrent.Name = "ClearCurrent";
+            this.ClearCurrent.Size = new System.Drawing.Size(137, 23);
+            this.ClearCurrent.TabIndex = 2;
+            this.ClearCurrent.Text = "Clear Current (c)";
+            this.ClearCurrent.UseVisualStyleBackColor = true;
+            this.ClearCurrent.Click += new System.EventHandler(this.ClearCurrent_Click);
+            // 
+            // Export
+            // 
+            this.Export.Location = new System.Drawing.Point(298, 668);
+            this.Export.Name = "Export";
+            this.Export.Size = new System.Drawing.Size(137, 23);
+            this.Export.TabIndex = 3;
+            this.Export.Text = "Export Country Data (e)";
+            this.Export.UseVisualStyleBackColor = true;
+            this.Export.Click += new System.EventHandler(this.Export_Click);
+            // 
+            // ClearLast
+            // 
+            this.ClearLast.Location = new System.Drawing.Point(155, 698);
+            this.ClearLast.Name = "ClearLast";
+            this.ClearLast.Size = new System.Drawing.Size(137, 23);
+            this.ClearLast.TabIndex = 4;
+            this.ClearLast.Text = "Clear Last (x)";
+            this.ClearLast.UseVisualStyleBackColor = true;
+            this.ClearLast.Click += new System.EventHandler(this.ClearLast_Click);
+            // 
+            // createCountries
+            // 
+            this.createCountries.AutoSize = true;
+            this.createCountries.Checked = true;
+            this.createCountries.Location = new System.Drawing.Point(869, 13);
+            this.createCountries.Name = "createCountries";
+            this.createCountries.Size = new System.Drawing.Size(103, 17);
+            this.createCountries.TabIndex = 5;
+            this.createCountries.TabStop = true;
+            this.createCountries.Text = "Create Countries";
+            this.createCountries.UseVisualStyleBackColor = true;
+            this.createCountries.CheckedChanged += new System.EventHandler(this.createCountries_CheckedChanged);
+            // 
+            // createConnections
+            // 
+            this.createConnections.AutoSize = true;
+            this.createConnections.Location = new System.Drawing.Point(869, 37);
+            this.createConnections.Name = "createConnections";
+            this.createConnections.Size = new System.Drawing.Size(162, 17);
+            this.createConnections.TabIndex = 6;
+            this.createConnections.TabStop = true;
+            this.createConnections.Text = "Choose Country Connections";
+            this.createConnections.UseVisualStyleBackColor = true;
+            this.createConnections.CheckedChanged += new System.EventHandler(this.createConnections_CheckedChanged);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(298, 698);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(137, 23);
+            this.Save.TabIndex = 7;
+            this.Save.Text = "Save Country Data (s)";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // BoardCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 803);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.createConnections);
+            this.Controls.Add(this.createCountries);
+            this.Controls.Add(this.ClearLast);
+            this.Controls.Add(this.Export);
+            this.Controls.Add(this.ClearCurrent);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.pictureBox1);
+            this.Location = new System.Drawing.Point(600, 200);
+            this.Name = "BoardCreator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Risk Board Creator";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Anything_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button ClearCurrent;
+        private System.Windows.Forms.Button Export;
+        private Button ClearLast;
+        private RadioButton createCountries;
+        private RadioButton createConnections;
+        private Button Save;
+    }
+}
+
---
+++ RiskMapMaker/RiskMapMaker/RiskBoardCreator.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace RiskMapMaker
+{
+    public partial class BoardCreator : Form
+    {
+        public BoardCreator()
+        {
+            InitializeComponent();
+
+            foreach (Control control in Controls)
+            {
+                control.KeyDown += new KeyEventHandler(Anything_KeyDown);
+                control.MouseMove += new MouseEventHandler(control_MouseMove);
+            }
+
+            Shown += new EventHandler(BoardCreator_Shown);
+            Load += new EventHandler(BoardCreator_Load);
+        }
+
+        void BoardCreator_Load(object sender, EventArgs e)
+        {
+            BringToFront();
+        }
+
+        void BoardCreator_Shown(object sender, EventArgs e)
+        {
+           BringToFront();            
+        }
+
+        class Country
+        {
+            public List<PointF> polyPoints = new List<PointF>();
+
+            public List<Country> countriesConnected = new List<Country>();
+            
+            /// <summary>
+            /// Set after it is fully made, and only then! (likely)
+            /// </summary>
+            public PointF centerPoint;
+
+            public bool IsIn(PointF point)
+            {
+                //First decompose poly to triangles and then check from there.
+                //This is not perfect as our decomposition will not be perfect, but its good enough for this!
+
+                //We just go through the polyPoints, instead of really creating the triangles
+                for (int x = 1; x < polyPoints.Count - 1; x++)                
+                    if (PointInTriangle(point, polyPoints[0], polyPoints[x], polyPoints[x + 1]))
+                        return true;
+
+                return false;
+            }
+
+            //Copied from http://www.blackpawn.com/texts/pointinpoly/default.html 
+            private static bool SameSide(PointF p1, PointF p2, PointF a, PointF b)
+            {
+                double cp1 = CrossProduct(Sub(b, a), Sub(p1, a));
+                double cp2 = CrossProduct(Sub(b, a), Sub(p2, a));
+                return (cp1 * cp2 >= 0);
+            }
+
+            private static bool PointInTriangle(PointF p, PointF a, PointF b, PointF c)
+            {
+                return (SameSide(p,a, b,c) && SameSide(p,b, a,c) && SameSide(p,c,a,b));        
+            }
+
+            public static double CrossProduct(PointF p1, PointF p2)
+            {
+                return p1.X * p2.Y - p1.Y * p2.X;
+            }
+
+            public static double DotProduct(PointF p1, PointF p2)
+            {
+                return p1.X * p2.X + p1.Y * p2.Y;
+            }
+
+            public static PointF Sub(PointF p1, PointF p2)
+            {
+                return new PointF(p1.X - p2.X, p1.Y - p2.Y);
+            }
+
+            public static PointF Add(PointF p1, PointF p2)
+            {
+                return new PointF(p1.X + p2.X, p1.Y + p2.Y);
+            }
+
+            public static PointF Mult(PointF p, float number)
+            {
+                return new PointF(p.X * number, p.Y * number);
+            }
+
+            public double Area()
+            {
+                double area = 0;
+
+                int i;
+                for (i = 0; i < polyPoints.Count - 1; i++)                
+                    area += (polyPoints[i].X * polyPoints[i + 1].Y - polyPoints[i + 1].X * polyPoints[i].Y);
+                area += (polyPoints[i].X * polyPoints[0].Y - polyPoints[0].X * polyPoints[i].Y);
+                area *= 0.5;
+
+                return area;
+            }
+
+            public PointF Centroid()
+            {
+                double area = Area();
+
+                double Cx = 0;
+                int i;
+                for (i = 0; i < polyPoints.Count - 1; i++)
+                {
+                    Cx += (polyPoints[i].X + polyPoints[i + 1].X) *
+                        ((polyPoints[i].X * polyPoints[i + 1].Y) - (polyPoints[i + 1].X * polyPoints[i].Y));
+                }
+
+                Cx += (polyPoints[i].X + polyPoints[0].X) *
+                        ((polyPoints[i].X * polyPoints[0].Y) - (polyPoints[0].X * polyPoints[i].Y));
+
+                Cx *= (1.0 / 6.0) / area;                
+
+                double Cy = 0;
+                for (i = 0; i < polyPoints.Count - 1; i++)
+                {
+                    Cy += (polyPoints[i].Y + polyPoints[i + 1].Y) *
+                        ((polyPoints[i].X * polyPoints[i + 1].Y) - (polyPoints[i + 1].X * polyPoints[i].Y));
+                }
+                Cy += (polyPoints[i].Y + polyPoints[0].Y) *
+                        ((polyPoints[i].X * polyPoints[0].Y) - (polyPoints[0].X * polyPoints[i].Y));
+
+                Cy *= (1.0 / 6.0) / area;
+
+                return new PointF((float)Cx, (float)Cy);
+            }
+        }
+    
+
+
+        States stateCur = States.Selecting;
+        enum States { Selecting, Connections };
+
+        List<Country> countries = new List<Country>();
+        Country countryCur = new Country();
+
+        PointF lastMousePosition;
+
+
+        //List<Country> countriesSelected = new List<Country>();
+        Country countrySelected = null;
+        Country lastSelected = null;
+
+        //Sort of hackish to make removing connections possible
+        List<Country> countriesWithConnectionsAdded = new List<Country>();
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs mouseEvent = (MouseEventArgs)e;
+            PointF pointCur = new PointF(mouseEvent.X, mouseEvent.Y);
+
+            if (stateCur == States.Selecting)
+            {
+                countryCur.polyPoints.Add(pointCur);
+            }
+            else if (stateCur == States.Connections)
+            {
+                lastSelected = countrySelected;
+                countrySelected = null;
+
+                //Look through countries to find the one selected
+                foreach(Country country in countries)
+                    if (country.IsIn(pointCur))
+                    {
+                        countrySelected = country;
+                        break;
+                    }                
+            }
+
+            Refresh();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (stateCur == States.Selecting)
+            {
+                //Fix up some stuff with countryCur
+
+                //Find countryCur center
+                if (countryCur.polyPoints.Count > 2)
+                {
+                    countryCur.centerPoint = countryCur.Centroid();
+
+                    countries.Add(countryCur);
+                }
+
+                countryCur = new Country();
+            }
+            else if (stateCur == States.Connections)
+            {
+                if (countrySelected != null)
+                {
+                    foreach (Country country in countries)
+                        if (country.IsIn(lastMousePosition))
+                        {
+                            countrySelected.countriesConnected.Add(country);
+                            country.countriesConnected.Add(countrySelected);
+                            break;
+                        }
+                    //Move it to back
+                    if(countriesWithConnectionsAdded.Contains(countrySelected))
+                        countriesWithConnectionsAdded.Remove(countrySelected);
+                    countriesWithConnectionsAdded.Add(countrySelected);
+                }
+            }
+
+            Refresh();
+        }
+
+        private void ClearCurrent_Click(object sender, EventArgs e)
+        {
+            if (stateCur == States.Selecting)
+            {
+                countryCur = new Country();
+            }
+            else if (stateCur == States.Connections)
+            {
+                countrySelected = null;
+                countries.Remove(countrySelected);
+            }
+            Refresh();
+        }
+
+        private void Export_Click(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void ClearLast_Click(object sender, EventArgs e)
+        {
+            if (stateCur == States.Selecting)
+            {
+                if (countryCur.polyPoints.Count > 0)
+                    countryCur.polyPoints.RemoveAt(countryCur.polyPoints.Count - 1);
+            }
+            else if (stateCur == States.Connections)
+            {
+                //Sort of hacking, but meh..
+                if (countriesWithConnectionsAdded.Count > 0)
+                {
+                    Country countryToRemove = countriesWithConnectionsAdded[countriesWithConnectionsAdded.Count - 1];
+
+                    for (int x = countryToRemove.countriesConnected.Count - 1; x >= 0; x--)
+                    {
+                        Country other = countryToRemove.countriesConnected[x];
+                        countryToRemove.countriesConnected.Remove(other);
+                        other.countriesConnected.Remove(countryToRemove);
+                    }
+                }
+            }
+            Refresh();
+        }
+
+        private void Anything_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.A: //Add
+                    Add_Click(null, null);
+                    break;
+                case Keys.C:
+                    ClearCurrent_Click(null, null);
+                    break;
+                case Keys.E:
+                    Export_Click(null, null);
+                    break;
+                case Keys.X:
+                    ClearLast_Click(null, null);
+                    break;
+                case Keys.S:
+                    Save_Click(null, null);
+                    break;
+            }
+        }
+
+        Color[] colors = { Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Indigo, Color.Violet };
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            for(int x = 0; x < countries.Count; x++)
+            {
+                Color color = colors[x % colors.Length];
+  
+                if(countries[x] == countrySelected)
+                    e.Graphics.FillPolygon(Brushes.White, countries[x].polyPoints.ToArray());
+                else
+                    e.Graphics.FillPolygon(new SolidBrush(color), countries[x].polyPoints.ToArray());       
+         
+                PointF[] box = new PointF[4];
+                box[0] = Country.Sub(countries[x].centerPoint, new PointF(10, 10));
+                box[1] = Country.Sub(countries[x].centerPoint, new PointF(-10, 10));
+                box[2] = Country.Sub(countries[x].centerPoint, new PointF(-10, -10));
+                box[3] = Country.Sub(countries[x].centerPoint, new PointF(10, -10));
+
+                e.Graphics.FillPolygon(Brushes.Black, box);
+            }
+
+            for (int x = 0; x < countries.Count; x++)
+            {
+                Color color = colors[x % colors.Length];
+
+                Pen connectedPen = new Pen(Color.FromArgb(200, Color.Firebrick), 5);
+                connectedPen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+                connectedPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                
+                foreach (Country connected in countries[x].countriesConnected)
+                {
+                    PointF direction = Country.Sub(countries[x].centerPoint, connected.centerPoint);
+
+                    PointF off = Country.Mult(direction, (float)(1.0 / (Math.Sqrt(direction.X * direction.X + direction.Y * direction.Y))));
+                    off = Country.Mult(off, 10);
+
+                    e.Graphics.DrawLine(connectedPen, connected.centerPoint, Country.Add(off,countries[x].centerPoint));
+                }
+            }
+
+            //Don't do last line, color last (current) specially (and the last point is not a line)
+            for (int x = 0; x < countryCur.polyPoints.Count - 1; x++)
+            {
+                e.Graphics.DrawLine(new Pen(Color.Olive, 5), countryCur.polyPoints[x], countryCur.polyPoints[x + 1]);
+            }
+            
+            //last part
+
+            if (stateCur == States.Selecting)
+            {
+                if (countryCur.polyPoints.Count > 0)
+                    e.Graphics.DrawLine(new Pen(Color.OrangeRed, 5),
+                        countryCur.polyPoints[countryCur.polyPoints.Count - 1],
+                        lastMousePosition);
+            }
+        }
+
+        void control_MouseMove(object sender, MouseEventArgs e)
+        {
+            lastMousePosition = new PointF(e.X, e.Y);
+
+            Refresh();
+        }
+
+        private void createCountries_CheckedChanged(object sender, EventArgs e)
+        {
+            if (createCountries.Checked)
+            {
+                stateCur = States.Selecting;
+            }
+            else
+            {
+                countryCur = new Country();
+            }
+        }
+
+        private void createConnections_CheckedChanged(object sender, EventArgs e)
+        {
+            if (createConnections.Checked)
+            {
+                stateCur = States.Connections;
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            BinaryFormater
+        }
+
+
+    }
+}
