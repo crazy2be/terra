dd4e15afc3fcb5f7812258011d6b067550904dea
crazy2be <crazy1be@gmail.com>
2012-05-27 17:01:52 -0400

    Turned off debug mode, and added Num field to Place() API call.

--- risk-server/place.go
+++ risk-server/place.go
@@ -8,6 +8,7 @@ import (
 
 type PlaceData struct {
 	Territory int
+	Num       int
 }
 
 func (g *Game) PlaceApi(r *http.Request) (interface{}, error) {
@@ -19,20 +20,23 @@ func (g *Game) PlaceApi(r *http.Request) (interface{}, error) {
 		return nil, err
 	}
 	
-	return nil, g.Place(pd.Territory)
+	return nil, g.Place(pd.Territory, pd.Num)
 }
 
-func (g *Game) Place(territory int) error {
+func (g *Game) Place(territory, num int) error {
 	if g.Turn.MenLeft <= 0 {
 		return fmt.Errorf("Sorry, you don't have any men to place!")
+	}
+	if g.Turn.MenLeft < num {
+		num = g.Turn.MenLeft
 	}
 	where := g.Territories[territory]
 	if where.Owner != g.Turn.Player {
 		return fmt.Errorf("Sorry, player %d isn't allowed to place in territory %d (that's owned by player %d)", g.Turn.Player, territory, where.Owner)
 	}
-	where.Men++
+	where.Men += num
 	where.Dirty()
-	g.Turn.MenLeft--
+	g.Turn.MenLeft -= num
 	g.Territories[territory] = where
 	if g.Turn.MenLeft == 0 {
 		g.Turn.Stage = "attacking"
